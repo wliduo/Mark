2ed835901132e3b80da1e081470b8d81b3d50202
wang926454 <1107224733@qq.com>
2019-01-18 20:55:05 +0800

    更新播放器

--- assets/wenkmPlayer/js/player.js
+++ assets/wenkmPlayer/js/player.js
@@ -1,8 +1,33 @@
-/* 根据明月浩空音乐播放器-免费版进行二次修改-wang926454 */
+/* 根据明月浩空音乐播放器-免费版进行二次修改-wang64.cn */
 var name = "随心";
-var domain = "wang64";
+var domain = "wang64.cn";
+
 // 获取我自己的歌单
 var aplist = getLove();
+
+// wangPalyer
+var parent = document.getElementById('wangPlayer');
+// 添加wenkmPlayer
+var div = document.createElement("div");
+div.setAttribute("id", "wenkmPlayer");
+div.innerHTML = '<div class="player"><div class="infos"><div class="songstyle"><i class="fa fa-music"></i><span class="song"></span></div><div class="timestyle"><i class="fa fa-clock-o"></i><span class="time">00:00/00:00</span></div><div class="artiststyle"><i class="fa fa-user"></i><span class="artist"></span><span class="moshi"><i class="loop fa fa-random current"></i><span class="moshi1">随机播放</span></span></div><div class="artiststyle"><i class="fa fa-folder"></i><span class="artist1"></span><span class="geci"></span></div></div><div class="control"><i class="loop fa fa-retweet"title="顺序播放"></i><i class="prev fa fa-backward"title="上一首"></i><div class="status"><b><i class="play fa fa-play"title="播放"></i><i class="pause fa fa-pause"title="暂停"></i></b></div><i class="next fa fa-forward"title="下一首"></i><i class="random fa fa-random current"title="随机播放"></i></div><div class="musicbottom"><div class="volume"><i class="mute fa fa-volume-off"></i><i class="volumeup fa fa-volume-up"></i><div class="progress"><div class="volume-on ts5"><div class="drag"title="音量"></div></div></div></div><div class="switch-playlist"><i class="fa fa-bars"title="播放列表"></i></div><div class="switch-ksclrc"><i class="fa fa-toggle-on"title="关闭歌词"></i></div><div class="switch-default"><i class="fa fa-refresh"title="切换默认专辑"></i></div></div><div class="cover"></div></div><div class="playlist"><div class="playlist-bd"><div class="album-list"><div class="musicheader"></div><div class="list"></div></div><div class="song-list"><div class="musicheader"><span></span></div><div class="list"><ul></ul></div></div></div></div><div class="switch-player"><i class="fa fa-angle-right"style="margin-top: 20px;"></i></div>';
+parent.appendChild(div);
+// 添加wenkmTips
+var div = document.createElement("div");
+div.setAttribute("id", "wenkmTips");
+parent.appendChild(div);
+// 添加wenkmLrc
+var div = document.createElement("div");
+div.setAttribute("id", "wenkmLrc");
+parent.appendChild(div);
+// 添加myhk_pjax_loading_frame
+var div = document.createElement("div");
+div.setAttribute("class", "myhk_pjax_loading_frame");
+parent.appendChild(div);
+// 添加myhk_pjax_loading
+var div = document.createElement("div");
+div.setAttribute("class", "myhk_pjax_loading");
+parent.appendChild(div);
 
 jQuery.cookie = function (name, value, options) {
 	if (typeof value != 'undefined') { // name and value given, set cookie
